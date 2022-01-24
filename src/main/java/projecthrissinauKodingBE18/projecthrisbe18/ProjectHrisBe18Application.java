96163e1b13dc98bb740df6cb4e25a80ce909698b
Hadiwasito1905 <noverryambo@gmail.com>
2022-01-24 16:31:38 +0700

    First Commit

---
+++ src/main/java/projecthrissinauKodingBE18/projecthrisbe18/ProjectHrisBe18Application.java
@@ -0,0 +1,13 @@
+package projecthrissinauKodingBE18.projecthrisbe18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectHrisBe18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectHrisBe18Application.class, args);
+	}
+
+}
---
+++ src/main/java/projecthrissinauKodingBE18/projecthrisbe18/entity/Attendance.java
@@ -0,0 +1,47 @@
+package projecthrissinauKodingBE18.projecthrisbe18.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "attendence")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Attendance extends BaseEntity<Attendance> {
+
+
+    private static final long serialVersionUID = -8207231947953017914L;
+
+
+    @Column(name = "date")
+    @Temporal(TemporalType.DATE)
+    private Date date;
+
+    @Column(name = "start_time")
+    @Temporal(TemporalType.TIME)
+    private Date startTime;
+
+    @Column(name = "end_time")
+    @Temporal(TemporalType.TIME)
+    private Date endTime;
+
+    @Column(name = "note", columnDefinition = "TEXT")
+    private String note;
+
+    @Column(name = "rest_start_time")
+    @Temporal(TemporalType.TIME)
+    private Date restStartTime;
+
+    @Column(name = "rest_end_time")
+    @Temporal(TemporalType.TIME)
+    private Date restEndTime;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "employee_id")
+    private Employee employee;
+}
